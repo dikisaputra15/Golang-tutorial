3b8fbbc1c84c9922e901084f26a3d614246681fa
Diki Center <dikimistak@gmail.com>
2022-01-06 13:48:58 +0700

    update tipe data string

---
+++ string.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("diki")
+	fmt.Println("saputra")
+	fmt.Println("kim jong diki")
+
+	fmt.Println(len("diki"))
+	fmt.Println("saputra"[0])
+	fmt.Println("kim jong diki"[1])
+}
