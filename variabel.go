4119e85cb12ca8da7a40d813bd72e56da7f63922
Diki Center <dikimistak@gmail.com>
2022-01-06 14:12:32 +0700

    variabel

---
+++ variabel.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+	var nama string
+
+	nama = "diki saputra"
+	fmt.Println(nama)
+
+	nama = "kim diki saputra"
+	fmt.Println(nama)
+
+	var (
+		namadepan    = "diki"
+		namabelakang = "low"
+	)
+	fmt.Println(namadepan)
+	fmt.Println(namabelakang)
+
+	friendname := "bareto"
+	fmt.Println(friendname)
+}
