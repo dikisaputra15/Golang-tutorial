b0021bc51296a7fb1e95cc19cf5c63ebb5954ca7
Diki Center <dikimistak@gmail.com>
2022-01-06 14:38:36 +0700

    constant

---
+++ constant.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	const (
+		firstname = "diki"
+		lastname  = "putra"
+		value     = 32
+	)
+
+	fmt.Println(firstname)
+	fmt.Println(lastname)
+	fmt.Println(value)
+}
