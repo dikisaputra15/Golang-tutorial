fc70f83e4bc70888a20e1a5a97941be8d79aa4e7
Diki Center <dikimistak@gmail.com>
2022-01-07 13:55:45 +0700

    operasi perbandingan

---
+++ perbandingan.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	var nama1 = "eko"
+	var nama2 = "eko"
+	var result bool = nama1 == nama2
+	fmt.Println(result)
+
+	var value1 = 100
+	var value2 = 200
+	fmt.Println(value1 < value2)
+	fmt.Println(value1 > value2)
+	fmt.Println(value1 == value2)
+	fmt.Println(value1 != value2)
+}
